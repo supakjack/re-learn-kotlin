b33b5e7e1cde940422dd9898c8bb4380652f6bc9
unknown <48548611+supakjack@users.noreply.github.com>
2021-11-10 21:42:00 +0700

    add
    add

--- my-app-a/app/src/main/java/com/example/myapplicationa/MainActivity.kt
+++ my-app-a/app/src/main/java/com/example/myapplicationa/MainActivity.kt
@@ -4,37 +4,70 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-    var cardView1 : CardView? = null
-    var cardView2 : CardView? = null
-    var cardView3 : CardView? = null
+    private val client = OkHttpClient()
+
+//    var cardView1 : CardView? = null
+//    var cardView2 : CardView? = null
+//    var cardView3 : CardView? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        cardView1 = findViewById<CardView>(R.id.CardView1)
-        cardView2 = findViewById<CardView>(R.id.CardView2)
-        cardView3 = findViewById<CardView>(R.id.CardView3)
+        run("https://order-plz.herokuapp.com/restaurants.json")
 
-        cardView1!!.setOnClickListener {
-            val intent = Intent(this,DetailActivity::class.java)
-            intent.putExtra("order","Blue")
-            startActivity(intent)
-        }
+//        cardView1 = findViewById<CardView>(R.id.CardView1)
+//        cardView2 = findViewById<CardView>(R.id.CardView2)
+//        cardView3 = findViewById<CardView>(R.id.CardView3)
 
-        cardView2!!.setOnClickListener {
-            val intent = Intent(this,DetailActivity::class.java)
-            intent.putExtra("order","Red")
-            startActivity(intent)
-        }
-
-        cardView3!!.setOnClickListener {
-            val intent = Intent(this,DetailActivity::class.java)
-            intent.putExtra("order","White")
-            startActivity(intent)
-        }
+//        cardView1!!.setOnClickListener {
+//            val intent = Intent(this,DetailActivity::class.java)
+//            intent.putExtra("order","Blue")
+//            startActivity(intent)
+//        }
+//
+//        cardView2!!.setOnClickListener {
+//            val intent = Intent(this,DetailActivity::class.java)
+//            intent.putExtra("order","Red")
+//            startActivity(intent)
+//        }
+//
+//        cardView3!!.setOnClickListener {
+//            val intent = Intent(this,DetailActivity::class.java)
+//            intent.putExtra("order","White")
+//            startActivity(intent)
+//        }
     }
+
+    fun run(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
+
+            var recyclerView: RecyclerView? = null
+
+            recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+            recyclerView!!.layoutManager = LinearLayoutManager(this)
+            val itemAdapter = MyAdapter(movies ,this)
+            recyclerView!!.adapter = itemAdapter
+
+        })
+    }
+
+
 }
